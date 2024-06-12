e71eb3f8bee229327cb75fa664d9c0d09fb2665c
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-06-12 17:09:53 -0500

    feat: :sparkles: Configuración de PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugIn } from './manifest.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 })
